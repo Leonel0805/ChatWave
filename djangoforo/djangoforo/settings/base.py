0d422ce104b16ed5e1820c3b2cc4e472660d7ec1
Leonel Rodriguez <114777251+Leonel0805@users.noreply.github.com>
2023-11-16 21:48:20 -0300

    update meet-people

--- djangoforo/djangoforo/settings/base.py
+++ djangoforo/djangoforo/settings/base.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -51,7 +52,7 @@ ROOT_URLCONF = 'djangoforo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join('templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,3 +110,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30)
+    
+}
