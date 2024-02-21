2c6ed8ffefee7910fb9e76da803329cb2604f851
= <leoulirodriguez@gmail.com>
2024-02-20 21:51:34 -0300

    fixing form registro

--- djangoforo/apps/core/forms.py
+++ djangoforo/apps/core/forms.py
@@ -9,3 +9,10 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User 
         fields = ('username', 'email', 'password')
+        
+        labels = {
+            'username':'Username:',
+            'email':'Email:',
+            'password':'Contraseña:'
+                
+        }
