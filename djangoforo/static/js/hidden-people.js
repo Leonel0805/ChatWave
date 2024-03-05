79449d6ec844c1799a4541c704ef178c7fceb41e
= <leoulirodriguez@gmail.com>
2024-03-05 18:52:11 -0300

    update responsive users-chat-online

--- djangoforo/static/js/hidden-people.js
+++ djangoforo/static/js/hidden-people.js
@@ -1,10 +1,5 @@
-console.log('hola desde js')
-
-console.log('room-chat-users-container')
-
 const iconBoton = document.getElementById('bi-people')
 const divHidden = document.getElementById('room-chat-users-container')
-console.log(divHidden)
 
 iconBoton.onclick = function(){
     if (divHidden.style.display === 'none' || divHidden.style.display === ''){
